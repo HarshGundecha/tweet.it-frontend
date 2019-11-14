28f52e79c5ca576fa36ccf558e3671bdfe17197f
HarshGundecha <harsh.gundecha@gmail.com>
2019-11-14 15:03:44 +0530

    add state management

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Router from './router';
+import Store from './store';
 import "@/assets/adminlte300-template/plugins/fontawesome-free/css/all.min.css";
 import "@/assets/adminlte300-template/dist/css/adminlte.min.css";
 import '@/assets/adminlte300-template/dist/js/adminlte.min.js';
@@ -9,4 +10,5 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
   router: Router, // Router added to the Vue instance
+  store:Store
 }).$mount('#app')
